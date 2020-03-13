7b15d98c8c9ed546ce01f1059bed79f55b04b3c5
Suleiman, Liana S <liana-suleiman@uiowa.edu>
2020-03-13 15:24:23 -0500

    intial commit

---
+++ customer/app/src/main/java/com/example/customer_application/EditModel.java
@@ -0,0 +1,14 @@
+package com.example.customer_application;
+//package com.example.parsaniahardik.recyclerview_edittext;
+
+public class EditModel {
+    private String editTextValue;
+
+    public String getEditTextValue() {
+        return editTextValue;
+    }
+
+    public void setEditTextValue(String editTextValue) {
+        this.editTextValue = editTextValue;
+    }
+}
---
+++ customer/app/src/main/java/com/example/customer_application/MainActivity.java
@@ -0,0 +1,4 @@
+package com.example.customer_application;
+
+public class MainActivity {
+}
