506e7fdfdc482af839923d158aa2eab9053f7c96
Sam <sam.morgan44@gmail.com>
2020-04-13 16:44:14 -0500

    adding linker class to other projects

---
+++ customer/app/src/main/java/com/example/myapplication/Linker.java
@@ -0,0 +1,330 @@
+package com.example.myapplication;
+
+
+
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Linker implements Runnable, Serializable {
+    private static LinkedBlockingQueue<String> q;
+    private static ArrayList<String> todoList;
+    private static ArrayList<Item> drinkItems;
+    private static ArrayList<Item> bbqItems;
+    private static ArrayList<Item> sideItems;
+    private static Connection connection;
+    private static int id;
+    private static boolean isWaiter;
+    private static Receipt receipt;
+    private static HashMap<Integer, Receipt> receiptMap;
+    private static HashMap<Integer, Integer> tableMap;
+    public static View currentView;
+
+    //going to pass in anything structure that can be updated over network
+    public Linker(int id, boolean isWaiter, ArrayList<String> todoList){
+        this.todoList = todoList;
+        drinkItems = new ArrayList<>();
+        bbqItems = new ArrayList<>();
+        sideItems = new ArrayList<>();
+        q = new LinkedBlockingQueue<>();
+
+        connection = new Connection("10.0.2.2", 4044, q);
+        connection.start();
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(this);
+
+        initializeItems();
+
+        Linker.id = id;
+        Linker.isWaiter = isWaiter;
+
+        if(isWaiter){
+            registerWaiter();
+            receiptMap = new HashMap<Integer, Receipt>();
+            tableMap = new HashMap<Integer, Integer>();
+        }else{
+            registerTable();
+        }
+    }
+
+    public static int getId(){
+        return id;
+    }
+
+
+    public static ArrayList<String> getTodoList(){
+        return todoList;
+    }
+
+    private void processItems(ArrayList<String> data){
+        if (data.get(0).equals("drink")){
+            drinkItems.clear();
+            fillItemList(data, drinkItems);
+        }
+        else if((data.get(0).equals("side"))){
+            sideItems.clear();
+            fillItemList(data, sideItems);
+        }
+        else if(data.get(0).equals("bbq")){
+            bbqItems.clear();
+            fillItemList(data, bbqItems);
+        }
+    }
+
+    private void fillItemList(ArrayList<String> data, ArrayList<Item> itemList){
+        itemList.add(new Item()); //empty item so index matches iid
+        for(int i=1; i<data.size(); i+=4){
+            Item item = new Item(Integer.parseInt(data.get(i)), data.get(i+1), Double.parseDouble(data.get(i+2)), data.get(i+3));
+
+            itemList.add(item);
+        }
+        System.out.println(itemList);
+    }
+
+    //used by tables
+    public static void orderBBQ(int iid, int quant){
+        sendMessage("order,bbq,"+iid+","+quant);
+        receipt.addItem(bbqItems.get(iid), quant);
+    }
+
+    public static void orderDrink(int iid, int quant){
+        sendMessage("order,drink,"+iid+","+quant);
+        receipt.addItem(drinkItems.get(iid), quant);
+    }
+
+    public static void orderSide(int iid, int quant){
+        sendMessage("order,side,"+iid+","+quant);
+        receipt.addItem(sideItems.get(iid), quant);
+    }
+
+    //used by waiters to add to certain tables receipt
+    public static void orderBBQ(int iid, int quant, int tid){
+        sendMessage("order,"+tid+",bbq,"+iid+","+quant);
+        receiptMap.get(tid).addItem(bbqItems.get(iid), quant);
+    }
+
+    public static void orderDrink(int iid, int quant, int tid){
+        sendMessage("order,"+tid+",drink,"+iid+","+quant);
+        receiptMap.get(tid).addItem(drinkItems.get(iid), quant);
+    }
+
+    public static void orderSide(int iid, int quant, int tid){
+        sendMessage("order,"+tid+",side,"+iid+","+quant);
+        receiptMap.get(tid).addItem(sideItems.get(iid), quant);
+    }
+
+    public static void requestUtensil(String utensil, int quant){
+        sendMessage("chop,"+utensil+","+quant);
+    }
+
+    public static void voidBBq(int iid, int quant, int tid){
+        sendMessage("void,"+tid+",bbq,"+iid+","+quant);
+        receiptMap.get(tid).addItem(bbqItems.get(iid), quant);
+    }
+
+    public static void voidDrink(int iid, int quant, int tid){
+        sendMessage("void,"+tid+",drink,"+iid+","+quant);
+        receiptMap.get(tid).addItem(drinkItems.get(iid), quant);
+    }
+
+    public static void voidUtensil(int iid, int quant, int tid){
+        sendMessage("void,"+tid+",side,"+iid+","+quant);
+        receiptMap.get(tid).addItem(sideItems.get(iid), quant);
+    }
+
+
+
+
+
+    public static void requestCheck(){
+        sendMessage("check");
+    }
+
+
+    private static void initializeItems(){
+        sendMessage("items");
+    }
+
+    private static void registerTable(){
+        sendMessage("register,table,"+id);
+    }
+
+    private static void registerWaiter(){
+        sendMessage("register,waiter,"+id);
+    }
+
+    public static void call(){
+        sendMessage("call");
+    }
+
+    public static void claim(int tid){
+        receiptMap.put(tid, new Receipt(tid, id));
+        tableMap.put(tid, id);
+        sendMessage("claim,"+tid);
+    }
+
+    public static void closeTable(int tid){
+        receiptMap.remove(tid);
+        tableMap.remove(tid);
+        sendMessage("close,"+tid);
+    }
+
+    public static void sendMessage(String message){
+        connection.sendData(message);
+    }
+
+    public static void printReceipt(){
+        if(isWaiter){
+            for (Receipt receipt : receiptMap.values()) {
+                if(receipt.getNumItems() > 0){
+                    System.out.println(receipt);
+                }
+            }
+        }else{
+            System.out.println(receipt);
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            if(!q.isEmpty()){
+                //get message
+                String request = q.poll();
+                String[] parts = request.split(",");
+
+                int tid = Integer.parseInt(parts[0]);
+                String command = parts[1];
+
+                ArrayList<String> data = new ArrayList<>();
+
+                if(parts.length > 2){
+                    for (int i = 2; i < parts.length; i++) {
+                        data.add(parts[i]);
+                    }
+                }
+
+                System.out.println("Table ID: " + tid + " || Request: " + command + " || Data: " + data);
+                if(!command.equals("items")){
+                    Snackbar.make(currentView, "Table ID: " + tid + " || Request: " + command + " || Data: " + data, Snackbar.LENGTH_LONG).show();
+                }
+
+                //general commands
+                if(command.equals("items")){
+                    processItems(data);
+                }
+                //...
+                //command is for waiter
+                else if(isWaiter){
+                    if(command.equals("call")){
+                        todoList.add("Table ID: " + tid + " || Request: " + command);
+                    }
+                    else if(command.equals("check")){
+                        todoList.add("Table ID: " + tid + " || Request: " + command);
+                    }
+                    else if(command.equals("order")){
+                        String category = "";
+                        Item item;
+                        int iid;
+                        int quant;
+                        for(int i=0; i<data.size(); i+=3){
+                            category = data.get(i);
+                            iid = Integer.parseInt(data.get(i+1));
+                            quant = Integer.parseInt(data.get(i+2));
+                            if(category.equals("drink")){
+                                item = drinkItems.get(iid);
+                                receiptMap.get(tid).addItem(item, quant);
+                            }
+                            else if(category.equals("bbq")){
+                                item = bbqItems.get(iid);
+                                receiptMap.get(tid).addItem(item, quant);
+                            }
+                            else if(category.equals("sides")){
+                                item = sideItems.get(iid);
+                                receiptMap.get(tid).addItem(item, quant);
+                            }
+                        }
+                    }
+                    else if(command.equals("chop")){
+                        todoList.add("Table ID: " + tid + " || Request: " + command);
+                    }
+                    else if(command.equals("claim")){
+                        int wid = Integer.parseInt(data.get(0));
+                        tableMap.put(tid, wid);
+                    }
+                    else if(command.equals("close")){
+                        tableMap.remove(tid);
+                    }
+                }
+                //command is for table
+                else{
+                    if(command.equals("claim")){
+                        int wid = Integer.parseInt(data.get(0));
+                        receipt = new Receipt(id,wid);
+                    }
+                    else if(command.equals("order")){
+                        String category = "";
+                        Item item;
+                        int iid;
+                        int quant;
+                        for(int i=0; i<data.size(); i+=3){
+                            category = data.get(i);
+                            iid = Integer.parseInt(data.get(i+1));
+                            quant = Integer.parseInt(data.get(i+2));
+                            if(category.equals("drink")){
+                                item = drinkItems.get(iid);
+                                receipt.addItem(item, quant);
+                            }
+                            else if(category.equals("bbq")){
+                                item = bbqItems.get(iid);
+                                receipt.addItem(item, quant);
+                            }
+                            else if(category.equals("sides")){
+                                item = sideItems.get(iid);
+                                receipt.addItem(item, quant);
+                            }
+                        }
+                    }
+                    else if(command.equals("void")){
+                        String category = "";
+                        Item item;
+                        int iid;
+                        int quant;
+                        for(int i=0; i<data.size(); i+=3){
+                            category = data.get(i);
+                            iid = Integer.parseInt(data.get(i+1));
+                            quant = Integer.parseInt(data.get(i+2));
+                            if(category.equals("drink")){
+                                item = drinkItems.get(iid);
+                                receipt.removeItem(item, quant);
+                            }
+                            else if(category.equals("bbq")){
+                                item = bbqItems.get(iid);
+                                receipt.removeItem(item, quant);
+                            }
+                            else if(category.equals("sides")){
+                                item = sideItems.get(iid);
+                                receipt.removeItem(item, quant);
+                            }
+                        }
+                    }
+                    else if(command.equals("close")){
+                        receipt = null;
+                    }
+                }
+
+
+                todoList.add("Table ID: " + tid + " || Request: " + command + " || Data: " + data);
+
+            }
+        }
+    }
+}
---
+++ waiter/app/src/main/java/waiter/myapplication/Connection.java
@@ -0,0 +1,179 @@
+package waiter.myapplication;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Connection implements Runnable {
+    private String ip;
+    private int port;
+    private Socket socket;
+    private BufferedWriter output;
+    private BufferedReader input;
+    private ExecutorService executor = Executors.newCachedThreadPool();
+    private ServerSender sender;
+    private ServerRequester requester;
+    private LinkedBlockingQueue<String> q;
+
+    public Connection(String ip, int port, LinkedBlockingQueue<String> q){
+        this.ip = ip;
+        this.port = port;
+        this.q = q;
+    }
+
+    public void start(){
+        executor.execute(this);
+        try{
+            executor.awaitTermination(1, TimeUnit.SECONDS);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        sender = new ServerSender(this, output);
+        requester = new ServerRequester(this, input);
+    }
+
+    public void connect() {
+        try{
+            this.socket = new Socket(InetAddress.getByName(ip),port);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.err.println(e);
+        }
+    }
+
+    public void initializeStreams(){
+        try{
+            output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            output.flush();
+
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Buffers successfully setup");
+        }catch (IOException e){
+            System.err.println("Error initializing buffers...");
+            System.err.println(e);
+        }
+    }
+
+    public void processConnection(){
+        while(true){
+            try{
+                String message = input.readLine();
+                if(message != null){
+                    System.out.println(message);
+                   q.offer(message);
+                }else{break;}
+            }catch (IOException e){
+                System.err.println(e);
+            }
+        }
+        closeConnection();
+    }
+
+    public void closeConnection(){
+        System.out.println("Attempting to close connection with server");
+        try{
+            socket.close();
+            input.close();
+            output.close();
+            System.out.println("Connection connection closed" );
+
+        }catch (IOException e){
+            System.err.println("Error closing client connection");
+            System.err.println(e);
+        }
+    }
+
+    public void sendData(String data){
+        sender.addData(data);
+        executor.execute(sender);
+    }
+
+    public String receiveData(){
+        try{
+            executor.execute(requester);
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+            return requester.getData();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    //Runnable interface
+    @Override
+    public void run() {
+        try{
+            connect();
+            initializeStreams();
+            processConnection();
+        }catch (Exception e){
+            System.err.println(e);
+        }
+    }
+
+    private class ServerRequester implements Runnable {
+        private Connection connection;
+        private BufferedReader input;
+        private String data;
+
+        public ServerRequester(Connection connection, BufferedReader input){
+            this.connection = connection;
+            this.input = input;
+            this.data = null;
+
+        }
+
+        public String getData(){
+            String returnData = data;
+            this.data = null;
+            return returnData;
+        }
+
+        @Override
+        public void run() {
+            try{
+                this.data = input.readLine();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ServerSender implements Runnable {
+        private Connection connection;
+        private BufferedWriter output;
+        private ArrayBlockingQueue<String> dataArray = new ArrayBlockingQueue<>(128);
+
+        public ServerSender(Connection connection, BufferedWriter output){
+            this.connection = connection;
+            this.output = output;
+        }
+
+        public void addData(String data){
+            dataArray.add(data);
+        }
+
+        @Override
+        public void run() {
+            while(!dataArray.isEmpty()){
+                try{
+                    String data = dataArray.poll();
+                    output.write(data+"\n");
+                    output.flush();
+                    System.out.println(data + " was sent...");
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
